82f421df3dfc0af9ced6e4170b4974d2e91f9eca
Diogo Araujo <diogo.araujo@wevo.com.br>
2020-08-28 22:04:54 -0300

    initial

---
+++ Eye Tractor/Eye Tractor/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Eye_Tractor.Services;
+using Eye_Tractor.Views;
+
+namespace Eye_Tractor
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Eye Tractor/Eye Tractor/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Eye_Tractor.Models;
+
+namespace Eye_Tractor.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
